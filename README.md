f837feb810f3e2d2fe170e25fcc0313892d7ff6a
github_piyush2108 <piyusharmap@gmail.com>
2023-12-02 19:11:52 +0530

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# 100xDevs Cohort
+
+This is a collection of all my work, including bounty assignments, projects, and more, done during Harkirat's 100xDevs cohort.
--- VideoJS Widevine/script.js
+++ VideoJS Widevine/script.js
@@ -1,7 +1,7 @@
 const chaptersToggle = document.getElementById("chapter-ctrl-btn");
 const chaptersContainer = document.getElementById("chapters-container");
-const videoShowcase = document.getElementById("vid-showcase");
 
+// to control video playblack using spacebar and arrow keys
 document.addEventListener("keydown", function (event) {
 	switch (event.key) {
 		case " ":
@@ -23,6 +23,7 @@ document.addEventListener("keydown", function (event) {
 	}
 });
 
+// to toggle the visibility of chapters container
 chaptersToggle.addEventListener("click", function () {
 	chaptersToggle.blur();
 	if (
@@ -37,6 +38,7 @@ chaptersToggle.addEventListener("click", function () {
 	}
 });
 
+// mock data
 let videoInfo = {
 	title: "Random Video Name",
 	duration: 210,
@@ -64,25 +66,6 @@ let videoInfo = {
 	],
 };
 
-let segments = [
-	{
-		title: "Chapter 1",
-		start: 0,
-	},
-	{
-		title: "Chapter 2",
-		start: 45,
-	},
-	{
-		title: "Chapter 3",
-		start: 130,
-	},
-	{
-		title: "Chapter 4",
-		start: 200,
-	},
-];
-
 var player = videojs(
 	"my-video",
 	{
@@ -107,12 +90,8 @@ var player = videojs(
 		});
 
 		player.on("loadedmetadata", function () {
-			segments.forEach(function (segment, index) {
-				segment.leftMargin = (segment.start / player.duration()) * 100;
-			});
-
-			addSegmentControls(player);
-			addSegmentTitles(player);
+			addChapterControls(player);
+			addChapterTitles(player);
 		});
 
 		player.ready(function () {
@@ -123,17 +102,13 @@ var player = videojs(
 	}
 );
 
-function addSegmentTitles(player) {
+function addChapterTitles(player) {
 	const chapterTitle = document.getElementById("chapter-titles");
 
-	segments.forEach(function (segment, index) {
+	videoInfo.segements.forEach(function (segment, index) {
 		const title = document.createElement("li");
 
 		title.innerHTML = segment.title;
-		title.style.fontSize = "1rem";
-		title.style.padding = "0.5rem";
-		title.style.listStyle = "none";
-		title.style.cursor = "pointer";
 
 		title.addEventListener("click", function () {
 			scrollToSegment(player, segment.start);
@@ -143,31 +118,30 @@ function addSegmentTitles(player) {
 	});
 }
 
-function addSegmentControls(player) {
+function addChapterControls(player) {
 	var seekBar = player.controlBar.progressControl.seekBar;
 
 	videoInfo.segements.forEach(function (segment) {
-		const segmentButton = document.createElement("button");
+		const segmentButton = document.createElement("span");
 		segmentButton.classList.add("chapter-break");
 
 		segmentButton.style.height = "100%";
+		// to calculate the width of chapter
 		segmentButton.style.width =
 			(segment.end / videoInfo.duration) * 100 -
 			(segment.start / videoInfo.duration) * 100 +
 			"%";
 		segmentButton.style.position = "absolute";
+		// to calculate left margin for the chapter
 		segmentButton.style.left =
 			(segment.start / videoInfo.duration) * 100 + "%";
 		segmentButton.style.border = "none";
-
-		segmentButton.addEventListener("click", function () {
-			scrollToSegment(player, segment.start);
-		});
 
 		seekBar.el().appendChild(segmentButton);
 	});
 }
 
+// to play the video at a particular time stamp
 function scrollToSegment(player, startTime) {
 	player.currentTime(startTime);
 	player.play();
